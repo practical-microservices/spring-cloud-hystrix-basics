7b20cde6303728eb3114cf05b0a07612a8fa40fc
Andrew Stakhov <astakhov@pivotal.io>
2018-06-21 12:01:51 -0400

    .NET implementation

---
+++ billboard-client-dotnetcore/Services/MessageService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using Steeltoe.CircuitBreaker.Hystrix;
+
+namespace BillboardClient.Services
+{
+    public class MessageService
+    {
+        private string DefaultMessage() => "Default Message: They don't make bugs like Bugs Bunny anymore";
+
+        public string GetMessage(string billboardId)
+        {
+            var cmd = new HystrixCommand<string>(HystrixCommandGroupKeyDefault.AsKey("GetMessage"), () => RunGetMessage(billboardId), DefaultMessage);
+            return cmd.Execute();
+        }
+        
+        private string RunGetMessage(string billboardId)
+        {
+            if( billboardId == null) {
+                throw new Exception("I'm totally not working - please fix me");
+            } else {
+                return "Hello is always a good start";
+            }
+        }
+
+        public string SlowOperation(string billboardId)
+        {
+            var commandOptions = new HystrixCommandOptions(HystrixCommandGroupKeyDefault.AsKey("SlowOperation"), HystrixCommandKeyDefault.AsKey("SlowOperation"))
+            {
+                CircuitBreakerEnabled = true, // Whether to use a HystrixCircuitBreaker or not.
+                CircuitBreakerForceClosed = false, // If true the HystrixCircuitBreaker.allowRequest() will always return true to allow requests regardless of the error percentage from HystrixCommandMetrics.getHealthCounts().
+                CircuitBreakerForceOpen = false, // If true the HystrixCircuitBreaker.allowRequest() will always return false, causing the circuit to be open (tripped) and reject all requests.
+                CircuitBreakerErrorThresholdPercentage = 50, // Error percentage threshold (as whole number such as 50) at which point the circuit breaker will trip open and reject requests.
+                CircuitBreakerRequestVolumeThreshold = 20, // Minimum number of requests in the metricsRollingStatisticalWindowInMilliseconds() that must exist before the HystrixCircuitBreaker will trip.
+                CircuitBreakerSleepWindowInMilliseconds = 5000, // The time in milliseconds after a HystrixCircuitBreaker trips open that it should wait before trying requests again.
+                ExecutionIsolationSemaphoreMaxConcurrentRequests = 10, // Number of concurrent requests permitted to HystrixCommand.run().
+                //ExecutionIsolationThreadPoolKeyOverride = "MyThreadpoolKey", // Allow a dynamic override of the HystrixThreadPoolKey that will dynamically change which HystrixThreadPool a HystrixCommand executes on.
+                ExecutionIsolationStrategy = ExecutionIsolationStrategy.THREAD, //What isolation strategy HystrixCommand.run() will be executed with.
+                ExecutionTimeoutEnabled = true, // Whether the timeout mechanism is enabled for this command
+                ExecutionTimeoutInMilliseconds = 1000, // Time in milliseconds at which point the command will timeout and halt execution.
+                FallbackEnabled = true, // whether fallback should be attempted when failure occurs.
+                FallbackIsolationSemaphoreMaxConcurrentRequests = 10, // Number of concurrent requests permitted to HystrixCommand.getFallback().
+                MetricsHealthSnapshotIntervalInMilliseconds = 500, // Time in milliseconds to wait between allowing health snapshots to be taken that calculate success and error percentages and affect HystrixCircuitBreaker.isOpen() status.
+                MetricsRollingPercentileBucketSize = 100, //Maximum number of values stored in each bucket of the rolling percentile.
+                MetricsRollingPercentileEnabled = true, // Whether percentile metrics should be captured using HystrixRollingPercentile inside HystrixCommandMetrics.
+                MetricsRollingPercentileWindowBuckets = 6, // Number of buckets the rolling percentile window is broken into.
+                MetricsRollingPercentileWindowInMilliseconds = 60000, // Duration of percentile rolling window in milliseconds.
+                MetricsRollingStatisticalWindowBuckets = 10, // Number of buckets the rolling statistical window is broken into.
+                MetricsRollingStatisticalWindowInMilliseconds = 10000, // Duration of statistical rolling window in milliseconds.
+                RequestCacheEnabled = true, // Whether AbstractCommand.getCacheKey() should be used with HystrixRequestCache to provide de-duplication functionality via request-scoped caching.
+                RequestLogEnabled = true, // Whether HystrixCommand execution and events should be logged to HystrixRequestLog.
+            };
+            var cmd = new HystrixCommand<string>(commandOptions, () => RunSlowOperation(billboardId), DefaultMessage);
+            return cmd.Execute();
+        }
+        
+        private string RunSlowOperation(string billboardId)
+        {
+            Thread.Sleep(2000);
+            return "It's a good day";
+        }
+    }
+}
---
+++ billboard-client-dotnetcore/Services/UserServiceCollapser.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BillboardClient.Models;
+using Microsoft.Extensions.Logging;
+using Steeltoe.CircuitBreaker.Hystrix;
+
+namespace BillboardClient.Services
+{
+
+    public class UserServiceCollapser : HystrixCollapser<List<User>, User, int>
+    {
+        readonly ILogger<UserServiceCollapser> _logger;
+
+        public static Dictionary<int, User> UserStore { get; set; } = new Dictionary<int, User>()
+        {
+            {1, new User() {Id = 1, Name = "Andrew"}},
+            {2, new User() {Id = 2, Name = "Alex"}},
+            {3, new User() {Id = 2, Name = "John"}},
+            {4, new User() {Id = 3, Name = "Bob"}},
+            {5, new User() {Id = 4, Name = "Seth"}},
+        };
+
+        public UserServiceCollapser(IHystrixCollapserOptions options, ILogger<UserServiceCollapser> logger) : base(options)
+        {
+            _logger = logger;
+        }
+
+        public int UserId { get; set; }
+
+        public override int RequestArgument => UserId;
+
+        protected override HystrixCommand<List<User>> CreateCommand(ICollection<ICollapsedRequest<User, int>> requests)
+        {
+            _logger.LogInformation("Creating batch command to handle {0} number of requests", requests.Count);
+            return new HystrixCommand<List<User>>(
+                group: HystrixCommandGroupKeyDefault.AsKey("UserCollapser"), 
+                run: () =>  RunBatch(requests), 
+                fallback: () => new List<User>() );
+        }
+        /**
+         *  A bulk loading method that looks up multiple users at the same time.
+         *  methods like this are useful when you can query multiple items from
+         *  a remote service faster than you can make individual queries
+         *  for each item.
+         *
+         * @param ids List of ID's to lookup
+         * @return A list User Objects if an user was not found null will be returned
+         */
+        /// <summary>
+        /// A bulk loading method that looks up multiple users at the same time.
+        /// methods like this are useful when you can query multiple items from
+        /// a remote service faster than you can make individual queries
+        /// for each item.
+        /// </summary>
+        /// <param name="requests">ids List of ID's to lookup</param>
+        /// <returns>A list User Objects if an user was not found null will be returned</returns>
+        private List<User> RunBatch(ICollection<ICollapsedRequest<User, int>> requests)
+        {
+            _logger.LogInformation($"SELECT * FROM Users WHERE Id IN ({string.Join(',',requests.Select(x => x.Argument))})");
+            return requests.Select(x => UserStore.GetValueOrDefault(x.Argument)).ToList();
+        }
+        
+        /// <summary>
+        /// Method that maps bulk response to the requests that went into it
+        /// </summary>
+        /// <param name="batchResponse">Collection of items that came back as part of bulk response</param>
+        /// <param name="requests">Orignal requests that need to be populated from bulk response</param>
+        protected override void MapResponseToRequests(List<User> batchResponse, ICollection<ICollapsedRequest<User, int>> requests)
+        {
+            var responses = batchResponse.ToDictionary(x => x.Id);
+            foreach (var request in requests)
+            {
+                request.Response = responses.GetValueOrDefault(request.Argument);
+            }
+        }
+    }
+}
